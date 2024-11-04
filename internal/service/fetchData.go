96404aff2439fdd818839f335736367a8f20762b
mhaatha <mhafidza528@gmail.com>
2024-11-04 16:00:33 +0800

    Make a request with Personal Access Token

--- internal/service/fetchData.go
+++ internal/service/fetchData.go
@@ -6,16 +6,37 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func GetUsername(username string) {
+	// Find .env file
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file: %s", err)
+	}
+
+	// Getting and using a value from .env
+	githubPAT := os.Getenv("PAT")
 	username = fmt.Sprintf("https://api.github.com/users/%s/events", username)
 
-	// Call the API
-	response, err := http.Get(username)
+	// Make request with the token
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", username, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	req.Header.Set("Authorization", "Bearer "+githubPAT)
+
+	// Call the API
+	response, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
 
 	// Read the responseData
 	responseData, err := io.ReadAll(response.Body)
