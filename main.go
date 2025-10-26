ff993148826b2d23edc1b6e8f53cbea7bdb64537
Hafidz Athaya <mhafidza528@gmail.com>
2025-10-26 17:16:17 +0800

    refactor: basic bubbletea code

--- main.go
+++ main.go
@@ -1,9 +1,75 @@
 package main
 
 import (
-	"github.com/mhaatha/gua-cli/cmd"
+	"fmt"
+	"log"
+
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
-	cmd.Execute()
+	p := tea.NewProgram(initialModel())
+	if _, err := p.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type (
+	errMsg error
+)
+
+type model struct {
+	textInput textinput.Model
+	err       error
+}
+
+func initialModel() model {
+	ti := textinput.New()
+	ti.Placeholder = "Pikachu"
+	ti.Focus()
+	ti.CharLimit = 156
+	ti.Width = 20
+
+	return model{
+		textInput: ti,
+		err:       nil,
+	}
+}
+
+// Pakai value receiver (bukan pointer receiver *model) karena
+// m akan dibuat baru, dengan cara:
+// 1. get salinannya
+// 2. update salinan tersebut
+// 3. return salinan tersebut
+// 4. m yang lama akan dibuang, digantikan dengan m salinan yang telah diupdate
+func (m model) Init() tea.Cmd {
+	return textinput.Blink
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyEnter, tea.KeyCtrlC, tea.KeyEsc:
+			return m, tea.Quit
+		}
+
+	case errMsg:
+		m.err = msg
+		return m, nil
+	}
+
+	m.textInput, cmd = m.textInput.Update(msg)
+	return m, cmd
+}
+
+func (m model) View() string {
+	return fmt.Sprintf(
+		"What’s your favorite Pokémon?\n\n%s\n\n%s",
+		m.textInput.View(),
+		"(esc to quit)",
+	) + "\n"
 }
