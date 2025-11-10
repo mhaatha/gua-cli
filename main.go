eefc222192132a62c2920206dcbf59032fb02f3c
Hafidz Athaya <mhafidza528@gmail.com>
2025-11-10 10:39:29 +0800

    learn: perform I/O operation

--- main.go
+++ main.go
@@ -1,142 +1,123 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"net/http"
 	"os"
+	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-// Shopping list TUI.
+// HTTP Request TUI.
 
 type model struct {
-	choices  []string         // items on the to-do list
-	cursor   int              // which to-do list item our cursor is pointing at
-	selected map[int]struct{} // which to-do list items are selected
+	status int    // HTTP response
+	err    error  // Possible error
+	url    string // URL string
 }
 
-// initialModel return our initial model
-// however, we could just as easily define the initial model
-// as a variabel elsewhere, too.
+// checkServer is a `Cmd` that makes a request to a server
+// and returns the result as a `Msg`
+//
+// `Cmd`s are functions that perform some I/O and then return a `Msg`.
+// Checking the time, ticking a timer, reading from a disk, etc
+// are all I/O and should be run through commands.
+func checkServer(url string) tea.Cmd {
+	return func() tea.Msg {
+		// Create an HTTP client and make a GET request.
+		c := &http.Client{Timeout: 10 * time.Second}
+		res, err := c.Get(url)
+
+		if err != nil {
+			// There was an error making our request. Wrap the error we received
+			// in a message and return it.
+			return errMsg{err}
+		}
+		// We received a response from the server. Return the HTTP status code
+		// as a message.
+		return statusMsg(res.StatusCode)
+	}
+}
+
+type statusMsg int
+
+type errMsg struct{ err error }
+
+// For messages that contain errors it's often handy to also implement the
+// error interface on the message.
+func (e errMsg) Error() string { return e.err.Error() }
+
+// We don't call the function
+// the Bubble Tea runtime will do that when the time is right.
+func (m model) Init() tea.Cmd {
+	return checkServer(m.url)
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case statusMsg:
+		// The server returned a status message. Save it to our model. Also
+		// tell the Bubble Tea runtime we want to exit because we have nothing
+		// else to do. We'll still be able to render a final view with our
+		// status message.
+		m.status = int(msg)
+		return m, tea.Quit
+
+	case errMsg:
+		// There was an error. Note it in the model. And tell the runtime
+		// we're done and want to quit.
+		m.err = msg
+		return m, tea.Quit
+
+	case tea.KeyMsg:
+		// Ctrl+c exits. Even with short running program it's good to have
+		// a quit key, just in case your logic is off. Users will be very
+		// annoyed if they can't exit.
+		if msg.Type == tea.KeyCtrlC {
+			return m, tea.Quit
+		}
+		if msg.Type == tea.KeyEsc {
+			return model{
+				err: errors.New("escape pressed"),
+			}, tea.Quit
+		}
+	}
+
+	// If we happen to get any other messages, don't do anything.
+	return m, nil
+}
+
+// View is very straightforward. We look at the current model
+// and build a string accordingly
+func (m model) View() string {
+	// If there's an error, print it out and don't do anything else.
+	if m.err != nil {
+		return fmt.Sprintf("\nWe had some trouble: %v\n", m.err)
+	}
+
+	// Tell the user we're doing something.
+	s := fmt.Sprintf("Checking %s ... ", m.url)
+
+	// When the server responds with a status, add it to the current line.
+	if m.status > 0 {
+		s += fmt.Sprintf("%d %s!", m.status, http.StatusText(m.status))
+	}
+
+	// Send off whatever we came up with above for rendering.
+	return "\n" + s + "\n\n"
+}
+
 func initialModel() model {
 	return model{
-		// Our to-do list is a grocery list
-		choices: []string{"Buy carrots", "Buy celery", "Buy kohlrabi"},
-
-		// A map which indicates which choices are selected. We're using
-		// the map like a mathematical set. The keys refer to the indexes
-		// of the `choices` slice, above.
-		selected: make(map[int]struct{}),
+		url: "https://charm.sh/",
 	}
 }
 
 func main() {
-	p := tea.NewProgram(initialModel())
-	if _, err := p.Run(); err != nil {
-		fmt.Printf("Alas, there's been an error: %v", err)
+	if _, err := tea.NewProgram(initialModel()).Run(); err != nil {
+		fmt.Printf("Uh oh, there was an error: %v\n", err)
 		os.Exit(1)
 	}
-}
-
-func (m model) Init() tea.Cmd {
-	// return nil means "no I/O right now, please."
-	return nil
-}
-
-// Will be called when "things happen."
-// Its job is to look at what has happened
-// and return an updated model in response.
-//
-// It can also return a `Cmd` to make more things happen.
-//
-// In this case, when a user pressed the down arrow
-// Update's job is to notice that the down arrow was pressed
-// and move the cursor accordingly (or not).
-//
-// The "something happened" comes in the form of a `Msg`, which can be any type.
-// Messages are the result of some I/O that took place
-// such as a keypress, timer tick, or a response from a server.
-//
-// We usually figure out which type of `Msg` we received with a type switch
-// but you could also use a type assertion.
-//
-// For now we'll just deal with tea.KeyMsg messages, which are automatically
-// sent to the update function when keys are pressed.
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-
-	// Is it a key press?
-	case tea.KeyMsg:
-
-		// Cool, what was the actually key pressed?
-		switch msg.String() {
-
-		// These keys should exit the program.
-		case "ctrl+c", "q", "esc":
-			return m, tea.Quit
-
-		// The "up" and "k" keys move the cursor up
-		case "up", "k":
-			if m.cursor > 0 {
-				m.cursor--
-			}
-
-		// The "down" and "j" keys move the cursor down
-		case "down", "j":
-			if m.cursor < len(m.choices)-1 {
-				m.cursor++
-			}
-
-		// The "enter" key and the spacebar (a literal space) toggle
-		// The selected state for the item that the cursor is pointing at.
-		case "enter", " ":
-			_, ok := m.selected[m.cursor]
-			if ok {
-				delete(m.selected, m.cursor)
-			} else {
-				m.selected[m.cursor] = struct{}{}
-			}
-		}
-	}
-
-	// Return the updated model to the Bubble Tea runtime for processing.
-	// Note that we're not returning a command.
-	return m, nil
-}
-
-// View is the simplest method.
-//
-// We look at the model in its current state and use it to return a `string`.
-// That string is our UI!
-//
-// Since the view describes the entire UI of the application
-// you don't have to worry about redrawing logic and stuff like that.
-// Bubble Tea takes care of it for you.
-func (m model) View() string {
-	// The header
-	s := "What should we buy at the market?\n\n"
-
-	// Iterate over our choices
-	for i, choice := range m.choices {
-
-		// Is the cursor pointing at this choice?
-		cursor := " " // no cursor
-		if m.cursor == i {
-			cursor = ">" // cursor!
-		}
-
-		// Is this choice selected?
-		checked := " " // not selected
-		if _, ok := m.selected[i]; ok {
-			checked = "x" // selected!
-		}
-
-		// Render the row
-		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
-	}
-
-	// The footer
-	s += "\nPress q or esc to quit.\n"
-
-	return s
 }
