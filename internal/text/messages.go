b75d577d6187fdffb368de4ab36ce2eb7b859401
Hafidz Athaya <mhafidza528@gmail.com>
2025-11-20 16:56:40 +0800

    add: const keywords for github events

--- internal/text/messages.go
+++ internal/text/messages.go
@@ -1,5 +1,20 @@
 package text
 
 const (
-	PushEvent = "PushEvent"
+	CommitCommentEvent            = "CommitCommentEvent"
+	CreateEvent                   = "CreateEvent"
+	DeleteEvent                   = "DeleteEvent"
+	DiscussionEvent               = "DiscussionEvent"
+	ForkEvent                     = "ForkEvent"
+	GollumEvent                   = "GollumEvent"
+	IssueCommentEvent             = "IssueCommentEvent"
+	IssuesEvent                   = "IssuesEvent"
+	MemberEvent                   = "MemberEvent"
+	PublicEvent                   = "PublicEvent"
+	PullRequestEvent              = "PullRequestEvent"
+	PullRequestReviewEvent        = "PullRequestReviewEvent"
+	PullRequestReviewCommentEvent = "PullRequestReviewCommentEvent"
+	PushEvent                     = "PushEvent"
+	ReleaseEvent                  = "ReleaseEvent"
+	WatchEvent                    = "WatchEvent"
 )
