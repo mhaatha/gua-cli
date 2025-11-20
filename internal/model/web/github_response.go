69e0638da9fef2a8cc0abd163511bc81d647b55b
Hafidz Athaya <mhafidza528@gmail.com>
2025-11-20 16:57:21 +0800

    add: github response struct

---
+++ internal/model/web/github_response.go
@@ -0,0 +1,43 @@
+package web
+
+import (
+	"encoding/json"
+	"time"
+)
+
+type GithubResponse struct {
+	ID        string    `json:"id"`
+	Type      string    `json:"type"`
+	Actor     Actor     `json:"actor"`
+	Repo      Repo      `json:"repo"`
+	Public    bool      `json:"public"`
+	CreatedAt time.Time `json:"created_at"`
+
+	// Org can be nil
+	Org *Org `json:"org,omitempty"`
+
+	Payload json.RawMessage `json:"payload"`
+}
+
+type Actor struct {
+	ID           int    `json:"id"`
+	Login        string `json:"login"`
+	DisplayLogin string `json:"display_login"`
+	GravatarID   string `json:"gravatar_id"`
+	URL          string `json:"url"`
+	AvatarURL    string `json:"avatar_url"`
+}
+
+type Repo struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
+type Org struct {
+	ID         int    `json:"id"`
+	Login      string `json:"login"`
+	GravatarID string `json:"gravatar_id"`
+	URL        string `json:"url"`
+	AvatarURL  string `json:"avatar_url"`
+}
