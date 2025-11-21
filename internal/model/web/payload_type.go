8fbf48a826743e972482be235135fb674433c50c
Hafidz Athaya <mhafidza528@gmail.com>
2025-11-21 13:56:16 +0800

    add: CreatePayload

--- internal/model/web/payload_type.go
+++ internal/model/web/payload_type.go
@@ -3,3 +3,14 @@ package web
 type CommitCommentPayload struct {
 	Action string `json:"action"`
 }
+
+type CreatePayload struct {
+	// Ref can be nil
+	Ref *string `json:"ref"`
+
+	RefType      string `json:"ref_type"`
+	FullRef      string `json:"full_ref"`
+	MasterBranch string `json:"master_branch"`
+	Description  string `json:"description"`
+	PusherType   string `json:"pusher_type"`
+}
